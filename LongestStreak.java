1039403888bc6ab55406e8e7f20243fc1bb30af0
Adam Menicucci <adam.menicucci@salesianum.org>
2023-11-30 19:13:03 -0500

    final

--- LongestStreak.java
+++ LongestStreak.java
@@ -2,29 +2,30 @@ public class LongestStreak {
     public static void main(String[] args) {
         Streak("CCAAAAATTT!"); //Should print out "A 5"
     }
-
     public static void Streak(String str) {
         //HOW TO LOOP OVER CHARACTERS IN A STRING?
         //HOW TO IDENTIFY A STREAK OF CHARACTERS? 
         //HOW TO TRACK THE LONGEST STREAK?
-        
         int high = 0;
-        char ch, ch2;
-        for (int i = 0; i < str.length(); i++) {
-            int tracker = 0;
-            for (int k = i; k < str.length(); k++) {
-                ch = str.charAt(i);
-                ch2 = str.charAt(k);
-                if (ch == ch2) {
+        int tracker = 0;
+        char maxChar = str.charAt(0);
+        for (int i = 1; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            char chMinus = str.charAt(i-1);
+            if (ch == chMinus) {
                 tracker++;
-                } else {
-                    break;
+            } else {
+                if (tracker > high) {
+                    high = tracker;
+                    maxChar = chMinus;
                 }
-            }
-            if (tracker > high) {
-                high = tracker;
+                tracker = 1;
             }
         }
-        System.out.println(high);
+        if (tracker > high) {
+            high = tracker;
+            maxChar = str.charAt(str.length()-1);
+        }
+        System.out.println(maxChar + " " + high);
     }
 }
